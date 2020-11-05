ec1db19600b091ba0c680bbe830bd00d9d851296
Sarah GP <sarah.groff.palermo@gmail.com>
2020-11-05 22:01:51 +0100

    delete more logs

--- src/createParser.js
+++ src/createParser.js
@@ -15,10 +15,4 @@ const grammar = readFileSync(grammarFilePath).toString();
 /* Run parser */
 const parser = generate(grammar);
 
-console.log(parser.parse('accumulation 5 lunge'));
-console.log(parser.parse('sometimes abba 2 walk 2 run'));
-console.log(parser.parse('often retrograde 2 jump'));
-console.log(parser.parse('sometimes coin_flip 3 walk 2 lunge'));
-
-
 exports.parser = parser;
